e3bd3e0a9210615204594a1f6599f214b56252c3
aryanverma007 <63182587+aryanverma007@users.noreply.github.com>
2020-05-20 19:13:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var database;
+var playerCount   ,   gameState = 0  ;
+var game,player,form  ;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw(){
+ 
+}
+
